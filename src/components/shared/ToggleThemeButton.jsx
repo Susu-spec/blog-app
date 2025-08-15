b4409fbc01eb50c0b56d180e737594b21cb540c4
Susu-spec <suleimansuwaibat@gmail.com>
2025-08-15 20:20:43 +0100

    fix: fixes theme not modifying on button click

--- src/components/shared/ToggleThemeButton.jsx
+++ src/components/shared/ToggleThemeButton.jsx
@@ -1,17 +1,17 @@
 import { IconButton } from "@chakra-ui/react";
-import { useColorMode } from "../ui/color-mode"
+import { ColorModeButton, useColorMode } from "../ui/color-mode"
 import { HiMoon, HiSun } from "react-icons/hi2";
 
 export default function ToggleThemeButton() {
     const { colorMode, toggleColorMode } = useColorMode();
 
     return (
-        <IconButton
+        <ColorModeButton
             aria-label="Toggle theme"
             onClick={toggleColorMode}
             variant="ghost"
         >
             {colorMode === "light" ? <HiMoon /> : <HiSun />}
-        </IconButton>
+        </ColorModeButton>
     )
 }
--- src/lib/theme.js
+++ src/lib/theme.js
@@ -2,7 +2,7 @@ import { defineConfig, createSystem, defaultConfig } from "@chakra-ui/react"
 
 const config = defineConfig({
   theme: {
-    tokens: {
+    semanticTokens: {
       colors: {
         bodyText: {
           value: { base: "#08090A", _dark: "#f7f8f8" },
@@ -11,20 +11,26 @@ const config = defineConfig({
           value: { base: "#f7f8f8", _dark: "#08090A" },
         },
         buttonText: {
-          value: { base: "#08090A", _dark: "#f7f8f8" },
+          value: { base: "#8a8f98", _dark: "#8a8f98" },
         },
         buttonActiveText: {
-          value: { base: "#f7f8f8", _dark: "#8a8f98" },
+          value: { base: "#0a0a0a", _dark: "#f7f8f8" },
         },
         buttonBg: {
-          value: { base: "#08090A", _dark: "#ffffff14" },
+          value: { base: "#ecedee", _dark: "#141516" },
+        },
+        buttonBorderColor: {
+          value: { base: "#e9e8ea", _dark: "#23252a" }
         },
         navBg: {
-          value: { base: "#fffc", _dark: "#0a0a0a" },
+          value: { base: "#fff", _dark: "#0a0a0a" },
         },
         navBorderBottom: {
           value: { base: "#eaeaea", _dark: "#333" }, // example fallback values
         },
+        linkHoverBg: {
+          value: { base: "#f7f8f8cc", _dark: "#0a0a0acc"}
+        }
       },
     },
   },
