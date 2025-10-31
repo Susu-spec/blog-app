e4967a568c6bcda321ba0f442c97c3abb0a76538
Susu-spec <suleimansuwaibat@gmail.com>
2025-10-31 19:01:32 +0100

    feat: adds `react-helmet` for dynamic page titles

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,21 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react"
 import { supabase } from "./lib/supabase"
 import { AuthProvider } from "./providers/AuthProvider"
 import { PostsProvider } from "./providers/PostsProvider"
+import { HelmetProvider } from "@dr.pogodin/react-helmet"
 
 function App() {
 
   return (
     <SessionContextProvider supabaseClient={supabase}>
       <AuthProvider>
-        <PostsProvider>
-          <AnimatePresence>
-            <RouterProvider router={routes} />
-            <Toaster />
-          </AnimatePresence>
-        </PostsProvider>
+        <HelmetProvider>
+          <PostsProvider>
+            <AnimatePresence>
+              <RouterProvider router={routes} />
+              <Toaster />
+            </AnimatePresence>
+          </PostsProvider>
+        </HelmetProvider>
       </AuthProvider>
     </SessionContextProvider>
     
