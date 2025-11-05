f361250b991842c19727cd39892d656d8070062f
Susu-spec <suleimansuwaibat@gmail.com>
2025-11-05 17:17:52 +0100

    feat: adds `existing-post` fixture; reduces total amount of fixtures needed for posts data

--- cypress/e2e/posts/create.cy.js
+++ cypress/e2e/posts/create.cy.js
@@ -4,52 +4,59 @@ describe("Create a Post", () => {
     before(() => {
         cy.fixture('users.json').then((data) => {
             userData = data.validUser;
+        });
+
+        cy.fixture("existing-post.json").then((data) => {
+            newPost = data;
         })
+    });
 
-        newPost = {
-            id: "1",
-            title: "New Post",
-            description: "New Post Description",
-            cover_image: "cypress/fixtures/blue-space.jpg",
-            content: "This is new content."
-        }
-    })
 
-    it("should fill and create a post successfully", () => {
-
+    beforeEach(() => {
         cy.login({
             email: userData.email,
             password: userData.password
-        })
+        });
+        cy.get('[data-cy="create-post-nav-mobile"]').click();
+        cy.url().should('eq', Cypress.config().baseUrl + "/post/create");
+        cy.contains(/create a new post/i).should('exist');
+    });
 
+
+    it("should fill and create a post successfully", () => {
         cy.fillPostForm({
             title: newPost.title,
             description: newPost.description,
-            cover_image: newPost.cover_image,
-            content: newPost.content
+            cover_image: "cypress/fixtures/blue-space.jpg",
+            content: "Updated content from Cypress test.",
         })
 
-        cy.intercept('POST', '**/rest/v1/posts*').as('createPost');
+        cy.intercept('POST', '**/rest/v1/posts*', {
+            statusCode: 201,
+            body: [newPost]
+        }).as('createPost');
+        cy.intercept(
+            'GET',
+            '**/rest/v1/posts?select=*%2Cauthor%3Aauthor_id%28name%29*&author_id=eq.*&order=created_at.desc',
+            {
+                statusCode: 200,
+                body: [newPost],
+            }
+        ).as('getMyPosts');
 
         cy.get('[data-cy="create-post-button"]').click();
 
         cy.wait("@createPost");
         cy.get('[data-cy=post-card]').should('exist');
-        cy.contains('New Post').should('be.visible');
-        cy.contains('New Post Description').should('be.visible');
-    })
+    });
+
 
     it('shows error when network fails during post creation', () => {
-        cy.login({
-            email: userData.email,
-            password: userData.password
-        })
-
         cy.fillPostForm({
             title: newPost.title,
             description: newPost.description,
-            cover_image: newPost.cover_image,
-            content: newPost.content
+            cover_image: "cypress/fixtures/blue-space.jpg",
+            content: "Updated content from Cypress test.",
         })
 
         cy.intercept('POST', '**/rest/v1/posts*', { forceNetworkError: true }).as('createPostFail');
@@ -58,12 +65,8 @@ describe("Create a Post", () => {
         cy.contains(/network error/i).should('be.visible');
     });
 
-    it('shows validation errors when fields are empty', () => {
-        cy.login({
-            email: userData.email,
-            password: userData.password
-        })
 
+    it('shows validation errors when fields are empty', () => {
         cy.get('[data-cy="create-post-nav-mobile"]').click();
         cy.get('textarea[name="title"]').focus().blur();
         cy.contains(/title is required/i).should('be.visible');
@@ -78,38 +81,34 @@ describe("Create a Post", () => {
         cy.contains(/Content is required/i).should('be.visible');
     });
 
+
+
     it('disables the create button when required fields are empty', () => {
-        cy.login({ 
-            email: userData.email, 
-            password: userData.password 
-        });
         cy.get('[data-cy="create-post-nav-mobile"]').click();
 
         cy.get('[data-cy="create-post-button"]').should('be.disabled');
 
+        cy.get('[data-cy="create-post-nav-mobile"]').click();
+        cy.url().should('eq', Cypress.config().baseUrl + "/post/create");
+        cy.contains(/create a new post/i).should('exist');
         cy.fillPostForm({
             title: newPost.title,
             description: newPost.description,
-            cover_image: newPost.cover_image,
-            content: newPost.content
+            cover_image: "cypress/fixtures/blue-space.jpg",
+            content: "Updated content from Cypress test.",
         });
 
         cy.get('[data-cy="create-post-button"]').should('not.be.disabled');
     });
 
+
+
     it('shows a conflict message when a post with the same title already exists', () => {
-        cy.login({ 
-            email: userData.email, 
-            password: userData.password 
-        });
-
-        cy.get('[data-cy="create-post-nav-mobile"]').click();
-
         cy.fillPostForm({
             title: newPost.title,
             description: newPost.description,
-            cover_image: newPost.cover_image,
-            content: newPost.content
+            cover_image: "cypress/fixtures/blue-space.jpg",
+            content: "Updated content from Cypress test.",
         });
 
         cy.intercept('POST', '**/rest/v1/posts*', {
